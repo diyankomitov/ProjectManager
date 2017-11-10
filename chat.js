e25f0eb92b449f2dd09bfd19aad4f4301cbcfcae
Diyan Komitov <mfb15140@uni.strath.ac.uk>
2017-11-10 01:04:39 +0000

    added more styling

--- chat.js
+++ chat.js
@@ -1,21 +1,23 @@
-// This is the client-side script.
 function test() {
-       // Initialize the HTTP request.
-       var xhr = new XMLHttpRequest();
-       xhr.open('get', 'chat.php');
+// This is the client-side script.
+    // Initialize the HTTP request.
+   var xhr = new XMLHttpRequest();
+    xhr.open('get', 'chat.php');
+    var chat = document.getElementById("chat");
+   // Track the state changes of the request.
+   xhr.onreadystatechange = function () {
+       var DONE = 4; // readyState 4 means the request is done.
+       var OK = 200; // status 200 is a successful return.
+       if (xhr.readyState === DONE) {
+           if (xhr.status === OK) {
+               chat.innerHTML += xhr.responseText; // 'This is the returned text.'
 
-       // Track the state changes of the request.
-       xhr.onreadystatechange = function () {
-           var DONE = 4; // readyState 4 means the request is done.
-           var OK = 200; // status 200 is a successful return.
-           if (xhr.readyState === DONE) {
-               if (xhr.status === OK) {
-                   document.getElementById("chat").innerHTML += xhr.responseText; // 'This is the returned text.'
-               } else {
-                   alert('Error: ' + xhr.status); // An error occurred during the request.
-               }
+               chat.scrollTop = chat.scrollHeight-chat.clientHeight;
+           } else {
+               alert('Error: ' + xhr.status); // An error occurred during the request.
            }
-       };
-       // Send the request to send-ajax-data.php
-       xhr.send(null);
+       }
+   };
+   // Send the request to send-ajax-data.php
+   xhr.send(null);
 }
