94311b133bc22417970a170b6125cf5260fce55a
Ewan Brown <ewan.brown.2015@uni.strath.ac.uk>
2017-11-24 14:56:49 +0000

    Ability to create projects added. Dates still dont work

--- projects.js
+++ projects.js
@@ -21,3 +21,31 @@ function projects_retrieveProjects() {
     // Send the request to send-ajax-data.php
     xhr.send(null);
 }
+
+function projects_createProject() {
+
+    // Initialize the HTTP request.
+    var xhr = new XMLHttpRequest();
+    var phpPage = 'projects_createProject.php';
+    var user = 'user=2';
+    var classs = 'class=' + document.getElementById("class").value;
+    var project = 'name=' + document.getElementById("name").value;
+    var description = 'description=' + document.getElementById("description").value;
+    var deadline = 'deadline=' + document.getElementById("deadline").value;
+    xhr.open('get', phpPage + '?' + user + '&' + classs + '&' + project + '&' + description + '&' + deadline);
+
+    // Track the state changes of the request.
+    xhr.onreadystatechange = function () {
+        var DONE = 4; // readyState 4 means the request is done.
+        var OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                window.location.href = 'index.html';
+            } else{
+                alert('Error: ' + xhr.status); // An error occurred during the request.
+            }
+        }
+    };
+    // Send the request to send-ajax-data.php
+    xhr.send(null);
+}
---
+++ projects_createProject.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: E
+ * Date: 18/11/2017
+ * Time: 23:45
+ */
+
+require_once "databaseConn.php";
+$conn = connectToDatabase();
+header('Content-Type: text/');
+
+$userId = $conn->real_escape_string($_GET["user"]);
+$class = $conn->real_escape_string($_GET["class"]);
+$name = $conn->real_escape_string($_GET["name"]);
+$description =$conn->real_escape_string($_GET["description"]);
+$deadline = $conn->real_escape_string($_GET["deadline"]);
+$deadline = null; //TODO: remove me when fixed dates
+
+$newProjectId = getNewProjectId($conn);
+$success = createProjectEntry($conn, $newProjectId, $class, $name, $description, 0, $deadline)
+    && createUserProjectEntries($conn, $userId, $newProjectId);
+
+if($success) echo "Success";
+
+//=============================================//
+//              Functions below                //
+//=============================================//
+
+function getNewProjectId($conn){
+
+    //Find the max id in projects. The new id is the max plus one.
+    $newProjectId = 1;
+    $result = $conn->query("SELECT max(`id`) FROM `Project`");
+    if($result->num_rows  > 0) {
+        $newProjectId = $result->fetch_array()[0] + 1;
+    } else if($conn->error) {
+        echo "ERROR at id:" . $conn->error;
+    }
+
+    return $newProjectId;
+}
+
+function createProjectEntry($conn, $id, $class, $name, $description, $isComplete, $deadline){
+
+    $sql = "";
+    if($description === null && $deadline === null){
+        $sql = "INSERT INTO `Project` (`id`, `class`, `name`, `is_complete`) 
+                VALUES ('$id', '$class', '$name', '$isComplete');";
+    } else if($description === null && $deadline !== null) {
+        $sql = "INSERT INTO `Project` (`id`, `class`, `name`, `is_complete`, `deadline`) 
+                VALUES ('$id', '$class', '$name', '$isComplete', '$deadline');";
+    } else if($description !== null && $deadline === null ){
+        $sql = "INSERT INTO `Project` (`id`, `class`, `name`, `description`, `is_complete`) 
+                VALUES ('$id', '$class', '$name', '$description', '$isComplete');";
+    } else{
+        $sql = "INSERT INTO `Project` (`id`, `class`, `name`, `description`, `is_complete`, `deadline`) 
+                VALUES ('$id', '$class', '$name', '$description', '$isComplete', '$deadline');";
+    }
+
+    if($conn->query($sql) === FALSE) {
+        echo "ERROR at project:" . $conn->error;
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
+function createUserProjectEntries($conn, $userId, $projectId){
+
+    $sql = "INSERT INTO `UserProject` (`user_id`, `project_id`, `is_read`) 
+                      VALUES ('$userId', '$projectId', '0')";
+
+    if($conn->query($sql) === FALSE){
+        echo "ERROR at userproject:" . $conn->error;
+        return false;
+    } else {
+        return true;
+    }
+}
