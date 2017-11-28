a5508f96be3a1c2d51ce35189d0ca217c3cd6250
Ewan Brown <ewan.brown.2015@uni.strath.ac.uk>
2017-11-28 19:03:24 +0000

    Fix: Users can no longer add a duplicate user to a project

--- scripts_AJAX/projects_addUser.php
+++ scripts_AJAX/projects_addUser.php
@@ -16,13 +16,41 @@ $email = $conn->real_escape_string($_GET['email']);
 $errors = ["-1"=>"ERROR QUERY MALFORMED",
     "-2"=>"There is no user by that address",
     "-3"=>"ERROR QUERY RETURNED MULTIPLE USERS",
-    "badInsert"=>"ERROR BAD INSERT"];
+    "badInsert"=>"ERROR BAD INSERT",
+    "userAlreadyInProject"=>"This user is already in this project"];
+
 
 $userId = findUser($conn, $email);
-if($userId < 0){
-    echo $errors[strval($userId)];
-} else if(addUserToProject($conn, $userId) === FALSE){
-    echo $errors["badInsert"];
+
+if(isUserInProject($conn, $userId)) {
+    echo $errors["userAlreadyInProject"];
+} else {
+    if ($userId < 0) {
+        echo $errors[strval($userId)];
+    } else if (addUserToProject($conn, $userId) === FALSE) {
+        echo $errors["badInsert"];
+    }
+}
+
+
+
+function isUserInProject($conn, $userId){
+    $result = $conn->query("SELECT `*` 
+                            FROM `UserProject` 
+                            WHERE `user_id` = $userId 
+                            AND `project_id` =  " . $_SESSION['selectedProjectId']);
+
+    if($result === FALSE){
+        echo $conn->error;
+    } else if($result->num_rows === 1){
+        //Already in project
+    } else if($result->num_rows > 1){
+        echo "ERROR at chat_sendMessage.php :: userNotInProject(): Multiple instances of user and project in UserProject.";
+    } else {
+        return false;
+    }
+
+    return true;
 }
 
 function findUser($conn, $email){
